e3d35d1fc4e024d9719b0cd57b077c8b9c27ff59
ploytisapcwt <pcwt.ti@hotmail.com>
2017-12-21 13:49:10 +0700

    3

--- ICT13580073FinalA/MainPage.xaml.cs
+++ ICT13580073FinalA/MainPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using ICT13580073FinalA.Model;
 using Xamarin.Forms;
 
 namespace ICT13580073FinalA
@@ -11,11 +11,40 @@ namespace ICT13580073FinalA
         {
             InitializeComponent();
             newButton.Clicked+= NewButton_Clicked;
+
         }
+		protected override void OnAppearing()
+		{
+			LoadData();
+		}
+		void LoadData()
+		{
+			productListView.ItemsSource = App.DbHelper.GetProducts();
+		}
 
         void NewButton_Clicked(object sender, EventArgs e)
         {
             Navigation.PushModalAsync(new ProductNewPage());
         }
+		void Edit_Clicked(object sender, System.EventArgs e)
+		{
+
+			var button = sender as MenuItem;
+			var product = button.CommandParameter as Product;
+			Navigation.PushModalAsync(new ProductNewPage(product));
+		}
+		async void Delete_Clicked(object sender, System.EventArgs e)
+		{
+			var isOk = await DisplayAlert("ยืนยัน", "คุณต้องการลบใช่หรือไม่", "ใช่", "ไม่ใช่");
+			if (isOk)
+			{
+				var button = sender as MenuItem;
+				var product = button.CommandParameter as Product;
+				App.DbHelper.DeleteProduct(product);
+
+				await DisplayAlert("ลบสำเร็จ", "ลบข้อมูลสินค้าเรียบร้อย", "ตกลง");
+				LoadData();
+			}
+		}
     }
 }
